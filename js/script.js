d184c500be8f4e16ac63d1ebecd7350db4e6c4e0
Anton Smyrnov <a.smyrnoff@gmail.com>
2016-10-31 00:33:17 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var names = [];
+  for (var i = 0; i < 5; i++) {
+    names[i] = prompt('Имя')
+  }
+  /*console.log (names);*/
+
+/*var user = prompt('Введите имя пользователя');
+  for (var i = 0; i < 5; i++) {
+    if (user == names[i]) {a = i};
+console.log (names[a])    */
+      
+
+
+

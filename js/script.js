ed6e727ccce8afbb035e39187548cb54516bc160
SmirnovA <a.smyrnoff@gmail.com>
2016-10-31 17:31:31 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-var names = [];
-  for (var i = 0; i < 5; i++) {
-    names[i] = prompt('Имя')
-  }
-  /*console.log (names);*/
 
-/*var user = prompt('Введите имя пользователя');
-  for (var i = 0; i < 5; i++) {
-    if (user == names[i]) {a = i};
-console.log (names[a])    */
+  var names = [];
+    for (var i = 0; i < 5; i++) {
+      names[i] = prompt('Имя №' + (i+1) + ' из списка')
+    }
+  var user = prompt('Введите имя пользователя');
+
+  function userCheck (login) 
+  {
+    
+    var result = 0;
+    for (var i = 0; i < 5; i++) 
+      {
+    	if (login == names[i]) 
+    	{
+    	  result = names[i];
+    	}
+
+      }
+
+      return result
+  }  
+
+  if (userCheck(user) == 0) {alert ('Ошибка. Данного пользователя не существует')}
+  	else {alert ('Вы успешно вошли, ' + userCheck(user))}
+  	
+
+
       
 
 

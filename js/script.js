8eac66f0afa9189aea7e9ca5f9125494f43333f5
AntonSmyrnoff <a.smyrnoff@gmail.com>
2016-11-03 16:15:49 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
 
   var names = [];
-    for (var i = 0; i < 5; i++) {
-      names[i] = prompt('Имя №' + (i+1) + ' из списка')
-    }
+  for (var i = 0; i < 5; i++) {
+     names[i] = prompt('Имя №' + (i+1) + ' из списка')
+  }
+
   var user = prompt('Введите имя пользователя');
+  if (userCheck(user, names) == 0) {alert ('Ошибка. Данного пользователя не существует')}
+  else {alert ('Вы успешно вошли, ' + userCheck(user, names))}
 
-  function userCheck (login) 
-  {
-    
+  function userCheck(login, arr) {
     var result = 0;
-    for (var i = 0; i < 5; i++) 
-      {
-    	if (login == names[i]) 
-    	{
-    	  result = names[i];
+    for (var i = 0; i < arr.length; i++) {
+    	if (login == arr[i]) {
+    	  result = arr[i];
     	}
-
-      }
-
-      return result
+    }
+  return result
   }  
 
-  if (userCheck(user) == 0) {alert ('Ошибка. Данного пользователя не существует')}
-  	else {alert ('Вы успешно вошли, ' + userCheck(user))}
+  if (userCheck(user, names) == 0) {alert ('Ошибка. Данного пользователя не существует')}
+  	else {alert ('Вы успешно вошли, ' + userCheck(user, names))}
   	
 
 
